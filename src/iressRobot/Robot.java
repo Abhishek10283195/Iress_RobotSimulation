52062fd44fc6bed7b6571014008ff3cd80e1d865
anag0014 <anag0014@student.monash.edu>
2022-07-06 12:55:14 +1000

    Initial commit

---
+++ src/iressRobot/Robot.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package iressRobot;
+
+/**
+ * Simulation of a toy robot on a square table top of 5*5 dimensions
+ * 
+ * 
+ * @author Abhishek Karthik
+ *
+ */
+public class Robot {
+
+	private int position;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
